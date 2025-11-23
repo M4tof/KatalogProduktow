3033a73076cf408f4aa2d8ee32d175d042a702de
Krzysztof Mańczak <krzysztfmanczak@gmail.com>
2025-11-23 15:12:42 +0100

    ReSetup

---
+++ ManczakSzybrua.KatalogProduktów.UI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ManczakSzybrua.KatalogProduktów.UI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ ManczakSzybura.KatalogProduktów.BL/BLMain.cs
@@ -0,0 +1,26 @@
+﻿using MańczakSzybura.KatalogProduktów.INTERFACES;
+using System.Reflection;
+
+namespace ManczakSzybura.KatalogProduktów.BL
+{
+    public class BLMain
+    {
+        public void setup()
+        {
+            //var dllPath = config["DaoLibrary"]; // odczyt z JSON
+            //var asm = Assembly.LoadFrom(dllPath);
+
+            //// Znajdź wszystkie klasy implementujące ICPU
+            //var cpuType = typeof(ICpu);
+            //var types = asm.GetTypes().Where(t => cpuType.IsAssignableFrom(t) && t.IsClass);
+
+            //foreach (var t in types)
+            //{
+            //    var cpu = (ICpu)Activator.CreateInstance(t);
+            //}
+
+
+
+        }
+    }
+}
